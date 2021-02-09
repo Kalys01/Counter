45c11df95f3e7127f42ec94784c450b0ed2159b9
Kalys01 <kalysosmon96.g@gmail.com>
2021-02-09 16:32:03 +0600

    added vuejs

--- main.js
+++ main.js
@@ -1,1 +1,10 @@
-console.log('hellio');
+const App = {
+    data() {
+        return {
+            counter: 0,
+            header: 'Counter'
+        }
+    }
+}
+
+Vue.createApp(App).mount('#app')
